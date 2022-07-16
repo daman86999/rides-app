877126e6f4ca638f92487ba6b9d236ac8ec81184
daman86999 <52445744+daman86999@users.noreply.github.com>
2022-07-16 13:01:00 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { React } from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+    </div>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { React } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://rides-app.hasura.app/v1/graphql',
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+);
+
+reportWebVitals();
