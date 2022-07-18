8aa4a79d50383be1beb6909e108532fef712d11a
daman86999 <52445744+daman86999@users.noreply.github.com>
2022-07-18 17:04:44 +0530

    removed unused hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { CircularProgress } from '@material-ui/core';
 import { ApolloProvider } from '@apollo/client';
 import Connection from './Connection';
-import useAccessToken from './hooks/useAcessToken';
+
 import { RoutesAll } from './routes/Routes';
 import Navbar from './components/Navbar';
 
 export default function App() {
   const { isLoading, isAuthenticated } = useAuth0();
-  const idToken = useAccessToken();
+
   if (isLoading) {
     return <CircularProgress />;
   }
 
-  const client = Connection(idToken);
+  const client = Connection();
   return (
     <ApolloProvider client={client}>
       <div>
